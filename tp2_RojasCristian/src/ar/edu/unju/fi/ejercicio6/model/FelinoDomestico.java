4df9f1588740e954da0d46446ac1a32bffb4458b
cristianrojas07 <cristianrdev@gmail.com>
2024-05-06 16:38:19 -0300

    ejercicio 6 listo

--- tp2_RojasCristian/src/ar/edu/unju/fi/ejercicio6/model/FelinoDomestico.java
+++ tp2_RojasCristian/src/ar/edu/unju/fi/ejercicio6/model/FelinoDomestico.java
@@ -5,4 +5,36 @@ public class FelinoDomestico {
 	private byte edad;
 	private float peso;
 	
+	public FelinoDomestico(String nombre, byte edad, float peso) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public byte getEdad() {
+		return edad;
+	}
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public String toString() {
+		return "FelinoDomestico [nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + "]";
+	}
+	
+	
 }
